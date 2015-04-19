73d7c7afc36ef5687e1b59ecc5c075b91a2d7eb1
Mangariu <M@mang.ariu>
2015-04-19 22:57:11 +0900

    Tutorial Lurp

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -12,7 +12,9 @@ public class Tutorial : MonoBehaviour {
 	public Button impossible;
 	public GameObject goal;
 	public GameObject blocks;
-
+	private bool clickPossible = false;
+	private bool clickImossible = false;
+	private SpriteRenderer[] srForColors;
 
 	// Use this for initialization
 	void Start () {
@@ -24,24 +26,48 @@ public class Tutorial : MonoBehaviour {
 	void Update () {
 		if (Time.time > 1){
 			goal.SetActive(true);
-			goalText.color = new Color(goalText.color.r,goalText.color.g,goalText.color.b,1.0f);
+			goalText.color = Color.Lerp(goalText.color, new Color(goalText.color.r,goalText.color.g,goalText.color.b,1.0f), 4.0f*Time.deltaTime);
+			srForColors = goal.GetComponentsInChildren<SpriteRenderer>();
+			foreach (SpriteRenderer sr in srForColors){
+				sr.color = Color.Lerp (sr.color, new Color(sr.color.r,sr.color.g,sr.color.b,1.0f), 4.0f*Time.deltaTime);
+			}
 		}
+
 		if (Time.time > 2){
 			blocks.SetActive(true);
-			blocksText.color = new Color(blocksText.color.r,blocksText.color.g,blocksText.color.b,1.0f);
+			blocksText.color = Color.Lerp(blocksText.color, new Color(blocksText.color.r,blocksText.color.g,blocksText.color.b,1.0f), 4.0f*Time.deltaTime);
+			srForColors = blocks.GetComponentsInChildren<SpriteRenderer>();
+			foreach (SpriteRenderer sr in srForColors){
+				sr.color = Color.Lerp (sr.color, new Color(sr.color.r,sr.color.g,sr.color.b,1.0f), 4.0f*Time.deltaTime);
+			}
 		}
 		if (Time.time >3){
-			otherText.color = new Color(otherText.color.r,otherText.color.g,otherText.color.b,1.0f);
+			otherText.color = Color.Lerp(otherText.color, new Color(otherText.color.r,otherText.color.g,otherText.color.b,1.0f), 4.0f*Time.deltaTime);
 		}
-		possible.onClick.AddListener(() => {
+		if (clickPossible){
 			blocksText.text = " ";
 			goalText.text = " ";
 			otherText.text = " ";
-			great.color = new Color(great.color.r,great.color.g,great.color.b,1.0f);
-			great.text = "Great!";
+			great.color = Color.Lerp (great.color, new Color(great.color.r,great.color.g,great.color.b,1.0f), 4.0f*Time.deltaTime);
+			great.text = "Great";
+		}
+		if (clickImossible){
+			blocksText.text = " ";
+			goalText.text = " ";
+			otherText.text = " ";
+			great.color = Color.Lerp (great.color, new Color(great.color.r,great.color.g,great.color.b,1.0f), 4.0f*Time.deltaTime);
+			great.text = "Try again!";
+		}
+
+		possible.onClick.AddListener(() => {
+			clickImossible = false;
+			great.color = new Color(great.color.r,great.color.g,great.color.b,0.0f);
+			clickPossible = true;
 		});
 		impossible.onClick.AddListener(() => {
-			great.text = "Try again!";
+			clickImossible = true;
+			great.color = new Color(great.color.r,great.color.g,great.color.b,0.0f);
+			clickPossible = false;
 		});
 	}
 
