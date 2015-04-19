55efe8f246e0388020526dea3d6a446885207184
Mangariu <M@mang.ariu>
2015-04-19 23:53:07 +0900

    Tuto rotation

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -15,6 +15,8 @@ public class Tutorial : MonoBehaviour {
 	private bool clickPossible = false;
 	private bool clickImossible = false;
 	private SpriteRenderer[] srForColors;
+	private float fadeinSpeed = 3.0f;
+
 
 	// Use this for initialization
 	void Start () {
@@ -26,38 +28,47 @@ public class Tutorial : MonoBehaviour {
 	void Update () {
 		if (Time.time > 1){
 			goal.SetActive(true);
-			goalText.color = Color.Lerp(goalText.color, new Color(goalText.color.r,goalText.color.g,goalText.color.b,1.0f), 4.0f*Time.deltaTime);
+			goalText.color = Color.Lerp(goalText.color, new Color(goalText.color.r,goalText.color.g,goalText.color.b,1.0f), fadeinSpeed*Time.deltaTime);
 			srForColors = goal.GetComponentsInChildren<SpriteRenderer>();
 			foreach (SpriteRenderer sr in srForColors){
-				sr.color = Color.Lerp (sr.color, new Color(sr.color.r,sr.color.g,sr.color.b,1.0f), 4.0f*Time.deltaTime);
+				sr.color = Color.Lerp (sr.color, new Color(sr.color.r,sr.color.g,sr.color.b,1.0f), fadeinSpeed*Time.deltaTime);
 			}
 		}
 
-		if (Time.time > 2){
+		if (Time.time > 3){
 			blocks.SetActive(true);
-			blocksText.color = Color.Lerp(blocksText.color, new Color(blocksText.color.r,blocksText.color.g,blocksText.color.b,1.0f), 4.0f*Time.deltaTime);
+			blocksText.color = Color.Lerp(blocksText.color, new Color(blocksText.color.r,blocksText.color.g,blocksText.color.b,1.0f), fadeinSpeed*Time.deltaTime);
 			srForColors = blocks.GetComponentsInChildren<SpriteRenderer>();
 			foreach (SpriteRenderer sr in srForColors){
-				sr.color = Color.Lerp (sr.color, new Color(sr.color.r,sr.color.g,sr.color.b,1.0f), 4.0f*Time.deltaTime);
+				sr.color = Color.Lerp (sr.color, new Color(sr.color.r,sr.color.g,sr.color.b,1.0f), fadeinSpeed*Time.deltaTime);
 			}
 		}
-		if (Time.time >3){
-			otherText.color = Color.Lerp(otherText.color, new Color(otherText.color.r,otherText.color.g,otherText.color.b,1.0f), 4.0f*Time.deltaTime);
+		if (Time.time >5){
+			otherText.color = Color.Lerp(otherText.color, new Color(otherText.color.r,otherText.color.g,otherText.color.b,1.0f), fadeinSpeed*Time.deltaTime);
+			//buttons "possible" and "impossible" should appear from here 
 		}
 		if (clickPossible){
 			blocksText.text = " ";
 			goalText.text = " ";
 			otherText.text = " ";
-			great.color = Color.Lerp (great.color, new Color(great.color.r,great.color.g,great.color.b,1.0f), 4.0f*Time.deltaTime);
+			great.color = Color.Lerp (great.color, new Color(great.color.r,great.color.g,great.color.b,1.0f), fadeinSpeed*Time.deltaTime);
 			great.text = "Great";
+			if(great.color.a > 0.9){
+				blocks.transform.rotation = Quaternion.Slerp(blocks.transform.rotation, Quaternion.Euler(new Vector3(0.0f,0.0f,-90.0f)),fadeinSpeed*Time.deltaTime);
+			}
 		}
 		if (clickImossible){
 			blocksText.text = " ";
 			goalText.text = " ";
 			otherText.text = " ";
-			great.color = Color.Lerp (great.color, new Color(great.color.r,great.color.g,great.color.b,1.0f), 4.0f*Time.deltaTime);
+			great.color = Color.Lerp (great.color, new Color(great.color.r,great.color.g,great.color.b,1.0f), fadeinSpeed*Time.deltaTime);
 			great.text = "Try again!";
 		}
+
+		if (true) {
+			//After clicking possible, both buttons should disappear and new button "Next" should appear
+		}
+
 
 		possible.onClick.AddListener(() => {
 			clickImossible = false;
