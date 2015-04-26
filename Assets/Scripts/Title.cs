519190c984e2ef99cb84862f72782821cc3ef8f8
Mangariu <M@mang.ariu>
2015-04-26 20:37:33 +0900

    title scene

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Title : MonoBehaviour {
+
+	public Button tutorial;
+	public Button gameStart;
+
+	// Use this for initialization
+	void Start () {
+		tutorial.onClick.AddListener(() => {
+			Application.LoadLevel("tutorial1");
+		});
+		
+		gameStart.onClick.AddListener(() => {
+			Application.LoadLevel("gameplay");
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
